58b81cd1149d3d341aaf3feacbd5e6853f0153c5
Issei Yoshida <i.yoshida1124@gmail.com>
2015-11-14 17:54:13 +0900

    途中

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,105 +1,106 @@
 angular.module('app.controllers', [])
 
 
-.controller('detailCtrl', function($scope) {
+  .controller('detailCtrl', function ($scope) {
 
-})
-
-
+  })
 
 
-
-.controller('feedbackCtrl', function($scope) {
-  $scope.active = 'happy';
-  $scope.setActive = function(type) {
+  .controller('feedbackCtrl', function ($scope) {
+    $scope.active = 'happy';
+    $scope.setActive = function (type) {
       $scope.active = type;
-  };
-  $scope.isActive = function(type) {
+    };
+    $scope.isActive = function (type) {
       return type === $scope.active;
-  };
+    };
 
-  $scope.save = function(feedback){
-    alert("debug: " + feedback.content + ":" + $scope.active);
-  }
-})
-
-
+    $scope.save = function (feedback) {
+      alert("debug: " + feedback.content + ":" + $scope.active);
+    }
+  })
 
 
+  .controller('timeLineCtrl', function ($scope, $ionicLoading, $timeout, $ionicPopover, $interval, locationService, parseService) {
 
-.controller('timeLineCtrl', function($scope,$ionicLoading,$timeout,$ionicPopover, locationService, parseService) {
 
-  $scope.$on('$ionicView.enter', function() {
-      $timeout(function() {
+    $scope.$on('$ionicView.enter', function () {
+      $timeout(function () {
         footerBar = document.body.querySelector('#TimeLineView .bar-footer');
         scroller = document.body.querySelector('#TimeLineView .scroll-content');
         txtInput = angular.element(footerBar.querySelector('textarea'));
       }, 0);
-  }, 20000);
+    }, 20000);
 
-  var footerBar;
-  var scroller;
+    var footerBar;
+    var scroller;
+    $scope.location = '読込中';
 
-  // 現在地更新
-  $scope.location = '読込中';
-  locationService.getCurrentLocation()
-    .then(function(results){
-      $scope.location = getLocationString(results[0].address_components);
-  });
+    $interval(function(){
+      $scope.location = locationService.getLocationName();
+    }, 1000);
 
-  // Googleから取得した現在地をいい感じに加工する
-  // サービスに移す
-  function getLocationString(address) {
-    var locality = address.filter(function(item,index){
-      if (item.types.indexOf('locality') >= 0) return true;
-    })[0].short_name;
+    $scope.debug = function(){
+      locationService.debug();
+    }
 
-    var sublocality = address.filter(function(item,index){
-      if (item.types.indexOf('sublocality_level_1') >= 0) return true;
-    })[0].short_name;
+    // 書き込みボタンをおした時に呼ばれる関数
+    // 必要な情報を集めてParseにPutする
+    $scope.sendMessage = function (sendMessageForm) {
+      parseService.savePost($scope.input.message);
+    }
 
-    return locality + sublocality + " 付近"
-  }
-
-  // 書き込みボタンをおした時に呼ばれる関数
-  // 必要な情報を集めてParseにPutする
-  $scope.sendMessage = function(sendMessageForm) {
-    parseService.savePost($scope.input.message);
-  }
-
-  $scope.datas = [
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "84kakvdi", "body": "理科大内で大・ケイドロ大会開催中！！！参加希望者はコメント下さい！！", "time": "10分", "comments": [{ "id": "84kakvdi", "body": "僕も参加したいです！次参加できるタイミングはいつですか？", "time": "6分" }, { "id": "75hvbfio", "body": "イスタンブール人も参加できる？", "time": "2分" }, { "id": "84kakvdi", "body": "参加したい方はルノアール前30分でよろしくっす！", "time": "1分" }] },
-        { "id": "ir93slksd", "body": "むこの授業が暇すぎる。どうしたものか。出席者で暇な人、咳してくれい", "time": "15分", "comments": [{ "id": "543kaaa", "body": "誰や、咳じゃなくてオナラしたやつ！！！", "time": "12分" }, { "id": "jk4ml4a", "body": "やめて、お茶ふいた", "time": "10分" }, { "id": "ir93slksd", "body": "ゆいちゃんペロペロ", "time": "1分" }] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] },
-        { "id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments":[] }
+    $scope.datas = [
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {
+        "id": "84kakvdi",
+        "body": "理科大内で大・ケイドロ大会開催中！！！参加希望者はコメント下さい！！",
+        "time": "10分",
+        "comments": [{"id": "84kakvdi", "body": "僕も参加したいです！次参加できるタイミングはいつですか？", "time": "6分"}, {
+          "id": "75hvbfio",
+          "body": "イスタンブール人も参加できる？",
+          "time": "2分"
+        }, {"id": "84kakvdi", "body": "参加したい方はルノアール前30分でよろしくっす！", "time": "1分"}]
+      },
+      {
+        "id": "ir93slksd",
+        "body": "むこの授業が暇すぎる。どうしたものか。出席者で暇な人、咳してくれい",
+        "time": "15分",
+        "comments": [{"id": "543kaaa", "body": "誰や、咳じゃなくてオナラしたやつ！！！", "time": "12分"}, {
+          "id": "jk4ml4a",
+          "body": "やめて、お茶ふいた",
+          "time": "10分"
+        }, {"id": "ir93slksd", "body": "ゆいちゃんペロペロ", "time": "1分"}]
+      },
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []},
+      {"id": "9osg4lias", "body": "あと３時間でアルバイトおおおお、めんどくさいいいいいああああ", time: "1分", "comments": []}
     ];
 
-  // 画面を引っ張った時の更新の関数
-  $scope.doRefresh = function() {
-    $ionicLoading.show({
-                template: 'Loading entries...'
-            });
-    $scope.$broadcast('scroll.refreshComplete');
-    $ionicLoading.hide();
-  };
+    // 画面を引っ張った時の更新の関数
+    $scope.doRefresh = function () {
+      $ionicLoading.show({
+        template: 'Loading entries...'
+      });
+      $scope.$broadcast('scroll.refreshComplete');
+      $ionicLoading.hide();
+    };
 
-  // ===========================
-  // 下部テキストエリアの
-  // ===========================
+    // ===========================
+    // 下部テキストエリアの
+    // ===========================
 
-  $scope.$on('taResize', function(e, ta) {
+    $scope.$on('taResize', function (e, ta) {
       if (!ta) return;
 
       var taHeight = ta[0].offsetHeight;
@@ -113,31 +114,31 @@ angular.module('app.controllers', [])
       scroller.style.bottom = newFooterHeight + 'px';
     });
 
-  // ===========================
-  // popover関係
-  // ===========================
-  $ionicPopover.fromTemplateUrl('my-popover.html', {
-    scope: $scope
-  }).then(function(popover) {
-    $scope.popover = popover;
-  });
-  $scope.openPopover = function($event) {
-    $scope.popover.show($event);
-  };
-  $scope.closePopover = function() {
-    $scope.popover.hide();
-  };
-  //Cleanup the popover when we're done with it!
-  $scope.$on('$destroy', function() {
-    $scope.popover.remove();
-  });
-  // Execute action on hide popover
-  $scope.$on('popover.hidden', function() {
-    // Execute action
-  });
-  // Execute action on remove popover
-  $scope.$on('popover.removed', function() {
-    // Execute action
-  });
+    // ===========================
+    // popover関係
+    // ===========================
+    $ionicPopover.fromTemplateUrl('my-popover.html', {
+      scope: $scope
+    }).then(function (popover) {
+      $scope.popover = popover;
+    });
+    $scope.openPopover = function ($event) {
+      $scope.popover.show($event);
+    };
+    $scope.closePopover = function () {
+      $scope.popover.hide();
+    };
+    //Cleanup the popover when we're done with it!
+    $scope.$on('$destroy', function () {
+      $scope.popover.remove();
+    });
+    // Execute action on hide popover
+    $scope.$on('popover.hidden', function () {
+      // Execute action
+    });
+    // Execute action on remove popover
+    $scope.$on('popover.removed', function () {
+      // Execute action
+    });
 
-})
+  })
--- www/js/services.js
+++ www/js/services.js
@@ -1,173 +1,221 @@
 angular.module('app.services', [])
 
-.factory('authService', [function(){
+  .factory('authService', [function () {
 
     function guid() {
-        function s4() {
-            return Math.floor((1 + Math.random()) * 0x10000)
-            .toString(16)
-            .substring(1);
-        }
-        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+      function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+          .toString(16)
+          .substring(1);
+      }
+
+      return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
     }
 
     function getUserId() {
-        if (window.localStorage) {
-            if (!window.localStorage.getItem('userid')) {
-                window.localStorage.setItem('userid', guid());
-            }
-            return window.localStorage.getItem('userid');
+      if (window.localStorage) {
+        if (!window.localStorage.getItem('userid')) {
+          window.localStorage.setItem('userid', guid());
         }
+        return window.localStorage.getItem('userid');
+      }
     }
 
     function getPasword() {
-        if (window.localStorage) {
-            if (!window.localStorage.getItem('userpassword')) {
-                window.localStorage.setItem('userpassword', guid());
-            }
-            return window.localStorage.getItem('userpassword');
+      if (window.localStorage) {
+        if (!window.localStorage.getItem('userpassword')) {
+          window.localStorage.setItem('userpassword', guid());
         }
+        return window.localStorage.getItem('userpassword');
+      }
     }
 
-    function signupToParse(userid, userpassword){
-        var user = new Parse.User();
-        user.set("username", userid);
-        user.set("password", userpassword);
+    function signupToParse(userid, userpassword) {
+      var user = new Parse.User();
+      user.set("username", userid);
+      user.set("password", userpassword);
 
-        user.signUp(null, {
-            success: function(user) {
-                console.log('debug: 初回ログイン、ユーザー作ったよ')
-            },
-            error: function(user, error) {
-                // Show the error message somewhere and let the user try again.
-                console.log("Error: " + error.code + " " + error.message);
-            }
-        });
+      user.signUp(null, {
+        success: function (user) {
+          console.log('debug: 初回ログイン、ユーザー作ったよ')
+        },
+        error: function (user, error) {
+          // Show the error message somewhere and let the user try again.
+          console.log("Error: " + error.code + " " + error.message);
+        }
+      });
     }
 
     function loginToParse(userid, password) {
-        Parse.User.logIn(userid, password, {
-            success: function(user) {
-                console.log('debug: 再ログイン、ログインしたよ')
-                console.log('success');
-            },
-            error: function(user, error) {
-                console.log("Error: " + error.code + " " + error.message);
-            }
-        });
+      Parse.User.logIn(userid, password, {
+        success: function (user) {
+          console.log('debug: 再ログイン、ログインしたよ')
+          console.log('success');
+        },
+        error: function (user, error) {
+          console.log("Error: " + error.code + " " + error.message);
+        }
+      });
     }
 
     function isUserExist(userid) {
-        var query = new Parse.Query(Parse.User);
-        query.equalTo("username",userid);
-        return query.find({
-            success: function(result){
-                if (result.length === 0) {
-                    return false;
-                }else{
-                    return true;
-                }
-            },
-            error: function(error){
-                console.log("Error:" + error.code + " " + error.message);
-            }
-        });
-    }
-
-    return {
-        login: function(){
-            // window.localStorage.clear();
-            var userid = getUserId();
-            var userpassword = getPasword();
-            console.log("UserId: " + userid);
-            isUserExist(userid).then(function(result){
-                if (result.length !== 0){
-                    loginToParse(getUserId(), getPasword());
-                }else{
-                    signupToParse(userid, userpassword);
-                }
-            });
-        }
-    }
-}])
-
-
-
-
-.factory('locationService', ['$q',function($q){
-    function getCurrentLatLng() {
-        var deferred = $q.defer();
-        var posOptions = {timeout: 10000, enableHighAccuracy: false};
-        navigator.geolocation.getCurrentPosition(deferred.resolve, deferred.reject, posOptions);
-        return deferred.promise;
-    };
-
-    function getLocation(position){
-        var deferred = $q.defer();
-        var latlng = new google.maps.LatLng(
-                        position.coords.latitude,
-                        position.coords.longitude);
-        var geocoder = new google.maps.Geocoder;
-        geocoder.geocode({'location': latlng}, deferred.resolve);
-        return deferred.promise;
-    };
-
-    return {
-        getCurrentLocation: function() {
-            return $q.when(getCurrentLatLng())
-            .then(getLocation);
+      var query = new Parse.Query(Parse.User);
+      query.equalTo("username", userid);
+      return query.find({
+        success: function (result) {
+          if (result.length === 0) {
+            return false;
+          } else {
+            return true;
+          }
         },
-
-        getCurrentLatLng: function(){
-            return getCurrentLatLng()
+        error: function (error) {
+          console.log("Error:" + error.code + " " + error.message);
         }
+      });
     }
-}])
+
+    return {
+      login: function () {
+        // window.localStorage.clear();
+        var userid = getUserId();
+        var userpassword = getPasword();
+        console.log("UserId: " + userid);
+        isUserExist(userid).then(function (result) {
+          if (result.length !== 0) {
+            loginToParse(getUserId(), getPasword());
+          } else {
+            signupToParse(userid, userpassword);
+          }
+        });
+      }
+    }
+  }])
 
 
+  .factory('locationService', ['$q', function ($q) {
+
+    var locationName = '読込中';
+    var position;
+    var posOptions = {timeout: 5000, enableHighAccuracy: false, maximumAge: 60000};
+    var id;
+
+    function getCurrentLatLng() {
+      var deferred = $q.defer();
+      navigator.geolocation.getCurrentPosition(deferred.resolve, deferred.reject, posOptions);
+      return deferred.promise;
+    };
+
+    function startWatchLocation(){
+      id = navigator.geolocation.watchPosition( successWatchLocation , errorWatchLocation , posOptions)
+      console.log(id);
+    };
+
+    function successWatchLocation(position){
+      this.position = position;
+      getLocation(position)
+        .then(function(results){
+          locationName = getLocationString(results[0].address_components);
+          console.log(locationName);
+        }
+      );
+    };
+
+    function errorWatchLocation(err){
+      console.error(err);
+    };
+
+    function getLocation(position) {
+      var deferred = $q.defer();
+      var latlng = new google.maps.LatLng(
+        position.coords.latitude,
+        position.coords.longitude);
+      var geocoder = new google.maps.Geocoder;
+      geocoder.geocode({'location': latlng}, deferred.resolve);
+      return deferred.promise;
+    };
+
+    // Googleから取得した現在地をいい感じに加工する
+    function getLocationString(address) {
+      var locality = address.filter(function (item, index) {
+        if (item.types.indexOf('locality') >= 0) return true;
+      })[0].short_name;
+
+      var sublocality = address.filter(function (item, index) {
+        if (item.types.indexOf('sublocality_level_1') >= 0) return true;
+      })[0].short_name;
+
+      return locality + sublocality + " 付近"
+    };
+
+    return {
+      getCurrentLocation: function () {
+        return $q.when(getCurrentLatLng())
+          .then(getLocation);
+      },
+
+      getCurrentLatLng: function () {
+        return getCurrentLatLng()
+      },
+
+      getLocationName: function () {
+        return locationName;
+      },
+
+      debug: function () {
+        //$q.when(getCurrentLatLng())
+        //  .then(getLocation)
+        //  .then(function (results) {
+        //    locationName = getLocationString(results[0].address_components);
+        //  });
+        startWatchLocation();
+      }
+
+    }
+  }])
 
 
-.factory('parseService',['locationService','$ionicLoading',function(locationService,$ionicLoading){
+  .factory('parseService', ['locationService', '$ionicLoading', function (locationService, $ionicLoading) {
 
     // 書き込み時日付とUserのオブジェクトIDから表示IDを作る
     // サービスに移す
-    function getMaskId(){
-        var currentUser = Parse.User.current();
-        var d = new Date();
+    function getMaskId() {
+      var currentUser = Parse.User.current();
+      var d = new Date();
 
-        var year = d.getFullYear();
-        var month = d.getMonth()+1;
-        var day = d.getDate();
+      var year = d.getFullYear();
+      var month = d.getMonth() + 1;
+      var day = d.getDate();
 
-        var seed = currentUser.id + year.toString() + month.toString() + day.toString();
-        var shaObj = new jsSHA("SHA-256", "TEXT");
-        shaObj.update(seed);
-        var hash = shaObj.getHash("HEX");
-        return hash.slice(-7);
+      var seed = currentUser.id + year.toString() + month.toString() + day.toString();
+      var shaObj = new jsSHA("SHA-256", "TEXT");
+      shaObj.update(seed);
+      var hash = shaObj.getHash("HEX");
+      return hash.slice(-7);
     };
 
     return {
-        savePost: function(content){
-            $ionicLoading.show({
-                template: '送信中'
-            });
-            locationService.getCurrentLatLng()
-            .then(function(position){
-                var point = new Parse.GeoPoint({latitude: position.coords.latitude, longitude: position.coords.longitude});
-                var currentUser = Parse.User.current();
-                var Post = Parse.Object.extend("Post");
-                var post = new Post();
-                post.set("maskid", getMaskId());
-                post.set("content", content);
-                post.set("location", point);
-                post.set("user", currentUser);
-                post.save()
-                .then(function(savedpost){
-                    console.log(JSON.stringify(savedpost));
-                    $ionicLoading.hide();
-                });
-            });
-        }
+      savePost: function (content) {
+        $ionicLoading.show({
+          template: '送信中'
+        });
+        locationService.getCurrentLatLng()
+          .then(function (position) {
+            var point = new Parse.GeoPoint({latitude: position.coords.latitude, longitude: position.coords.longitude});
+            var currentUser = Parse.User.current();
+            var Post = Parse.Object.extend("Post");
+            var post = new Post();
+            post.set("maskid", getMaskId());
+            post.set("content", content);
+            post.set("location", point);
+            post.set("user", currentUser);
+            post.save()
+              .then(function (savedpost) {
+                console.log(JSON.stringify(savedpost));
+                $ionicLoading.hide();
+              });
+          });
+      }
     }
-}]);
+  }]);
 
